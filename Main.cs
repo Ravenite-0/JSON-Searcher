036dd28ad9377486dab7252a2459fb42eb24e47c
hzp1997 <hzp1997@live.com>
2021-06-12 01:09:53 +1000

    Added main for debugging

---
+++ Main.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Main {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
