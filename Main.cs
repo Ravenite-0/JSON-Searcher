560f4c61fb17c1dcca5eee9fac11d689ef5e388f
hzp1997 <hzp1997@live.com>
2021-06-12 01:46:11 +1000

    Added filepath extractor

--- Main.cs
+++ Main.cs
@@ -1,10 +1,21 @@
 using System;
+using static Util.FileUtils;
+using static System.IO.Directory;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Main {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            string json_filepath = GetChildDir("json");            
+            List<string> files = GetFiles(json_filepath).Select(file => {
+                Console.WriteLine($"Importing from {file}...");
+                return file;
+            }).ToList();
+
+            Console.WriteLine("All files have been imported!");
+            
+            
         }
     }
 }
--- util/FileUtils.cs
+++ util/FileUtils.cs
@@ -1,5 +1,22 @@
+using static System.Reflection.Assembly;
+using static System.IO.Directory;
+using static System.IO.Path;
 namespace Util {
     public class FileUtils {
-        
+        //This method looks for the relative filepath of "json" folder in this repo.
+        public static string GetChildDir(string childDir) {
+            string targetpath = GetExecutingAssembly().Location;
+            string result = null;
+            while(GetParent(targetpath) != null) {
+                targetpath = GetParent(targetpath).FullName;
+                result = Combine(targetpath, childDir);
+                if(Exists(result)) {
+                    break;
+                } else {
+                    result = null;
+                }
+            }
+            return result;
+        }
     }
 }
