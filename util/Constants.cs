424c13ab9280e8b87df20b359422ac13e4f34f54
hzp1997 <hzp1997@live.com>
2021-06-12 23:20:31 +1000

    Interim check-in

--- util/Constants.cs
+++ util/Constants.cs
@@ -7,13 +7,11 @@ namespace Utils {
                             TBL_ORGANIZATION = "ORGANIZATION",
                             TBL_TICKET = "TICKET",
                             TBL_USER = "USER";
-        
-        public const string HELP_STR = 
-$@"help                                             Show command line help.
+        public static bool exit = false;
+        public const string HELP_STR = $@"
+help                                                Show command line help.
 clear                                               Clears the console panel.
 reload                                              Re-imports data from the JSON files.
 search table field value [field value]...           Searches a specific table using at least 1 field with a specific value.";
-
-        public static bool exit = false;
     }
 }
