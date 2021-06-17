d495fec5df67ca0457caa1f7ca4ca614f5653a2c
hzp1997 <hzp1997@live.com>
2021-06-18 02:05:48 +1000

    Test structure update

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests {
+  abstract class UnitTest {
+    public abstract class ConsoleUtilsTest {
+      [Test]
+      public void Test_SetConsoleTextColor() {
+        Assert.True(1 == 1);
+      }
+    }
+
+  }
+}
