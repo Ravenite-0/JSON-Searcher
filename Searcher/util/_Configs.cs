475fcfacd0fed22111f5752dedc1d3ac6b677bae
hzp1997 <hzp1997@live.com>
2021-06-18 00:03:31 +1000

    gitignore update

--- Searcher/util/_Configs.cs
+++
@@ -1,7 +0,0 @@
-namespace Utils {
-  public abstract class Config {
-    public static bool closeApp = false,
-                       debugMode = false,
-                       logging = true;
-  }
-}
